f5aa134f761d025427c2cd2b58520f730188939f
Rahul Verma <rahulsi9810@gmail.com>
2026-07-22 21:00:59 +0530

    feat: implement responsive job card component with styling and structure

--- day_3_mini_project/src/App.jsx
+++ day_3_mini_project/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <div className="center">
      <h3>Amazon <span>5 days ago</span>
      </h3>
-     <h2>Senior UI/UX Designe</h2>
+     <h2>Senior UI/UX Designer</h2>
      <div>
       <h4>Part Time</h4>
       <h4>Senior Level</h4>
